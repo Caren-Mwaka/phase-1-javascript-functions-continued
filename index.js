8ac4c1a44f4a60a0954dd9976a7ea2b863401926
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-22 08:22:01 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    if (activity === "bathe my dog") {
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog")); 
+
+function mondayWork(activity = 'go to the office') {
+    if (activity === "work from home") {
+        return `This Monday, I will ${activity}.`;
+    } else {
+        return "This Monday, I will go to the office.";
+    }
+}
+console.log(mondayWork()); 
+console.log(mondayWork("bathe my dog")); 
+
+function wrapAdjective(symbol = '*') {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+let result = wrapAdjective('*');
+let emphatic = result("a hard worker");
+console.log(emphatic); 
